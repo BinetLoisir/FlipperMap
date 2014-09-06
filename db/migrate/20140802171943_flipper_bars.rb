65ceccce2d955d9aaddf9671e4dc1e0782a33007
Matthieu Gendreau <matthieu.gendreau@pricematch.travel>
2014-09-06 02:28:17 +0200

    Fix migrations

--- db/migrate/20140802171943_flipper_bars.rb
+++ db/migrate/20140802171943_flipper_bars.rb
@@ -5,15 +5,16 @@ class FlipperBars < ActiveRecord::Migration
       t.belongs_to :bar
       t.timestamps
     end
-    execute 'ALTER TABLE `locations` ADD CONSTRAINT locations_ibfk_1
+
+    execute 'ALTER TABLE locations ADD CONSTRAINT locations_ibfk_1
       FOREIGN KEY(flipper_id) REFERENCES flippers(id) ON DELETE CASCADE ON UPDATE CASCADE'
-    execute 'ALTER TABLE `locations` ADD CONSTRAINT locations_ibfk_2
+    execute 'ALTER TABLE locations ADD CONSTRAINT locations_ibfk_2
       FOREIGN KEY(bar_id) REFERENCES bars(id) ON DELETE CASCADE ON UPDATE CASCADE'
   end
 
   def down
     drop_table :locations
-    execute 'ALTER TABLE `rival_rooms` DROP FOREIGN KEY locations_ibfk_1'
-    execute 'ALTER TABLE `rival_rooms` DROP FOREIGN KEY locations_ibfk_2'
+    execute 'ALTER TABLE locations DROP FOREIGN KEY locations_ibfk_1'
+    execute 'ALTER TABLE locations DROP FOREIGN KEY locations_ibfk_2'
   end
 end
